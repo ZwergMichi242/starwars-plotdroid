2a16a2e02d1c50190bf210d8feddcbfa33859095
ZwergMichi242 <michaela.anzengruber@antz-development.at>
2019-10-30 17:41:28 +0100

    Updates

---
+++ precache-manifest.7671a67184a6353163f7435ade1f74cf.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ada9eaa2720d8c3e2b96682b6023160d",
+    "url": "/starwars-plot-droide/index.html"
+  },
+  {
+    "revision": "29b3cdb90cb3848a4731",
+    "url": "/starwars-plot-droide/static/css/main.911e2425.chunk.css"
+  },
+  {
+    "revision": "82c6cfdc7a8ca2f5d7dc",
+    "url": "/starwars-plot-droide/static/js/2.4bf34477.chunk.js"
+  },
+  {
+    "revision": "29b3cdb90cb3848a4731",
+    "url": "/starwars-plot-droide/static/js/main.44e02245.chunk.js"
+  },
+  {
+    "revision": "e2e69b4176b31005eb44",
+    "url": "/starwars-plot-droide/static/js/runtime-main.3e9b825a.js"
+  },
+  {
+    "revision": "c982ef251eeaa93f501080ca646e5e24",
+    "url": "/starwars-plot-droide/static/media/anakin.c982ef25.ttf"
+  },
+  {
+    "revision": "ade91cb0bb7c69696a7df18125ae82cb",
+    "url": "/starwars-plot-droide/static/media/deathstar.ade91cb0.otf"
+  },
+  {
+    "revision": "7ba6f280fc3117e9cd133bf1c23aa95a",
+    "url": "/starwars-plot-droide/static/media/distant.7ba6f280.ttf"
+  },
+  {
+    "revision": "a9df3614dac722fd5ee0866953e0551b",
+    "url": "/starwars-plot-droide/static/media/droid.a9df3614.png"
+  },
+  {
+    "revision": "c998b685a8fdce0dcb4f7a35277a5b80",
+    "url": "/starwars-plot-droide/static/media/droid.c998b685.ttf"
+  },
+  {
+    "revision": "10c1a1443d862233730f1f7e388e8375",
+    "url": "/starwars-plot-droide/static/media/episodeone.10c1a144.ttf"
+  },
+  {
+    "revision": "75c0119ca475dea587b8661501320be5",
+    "url": "/starwars-plot-droide/static/media/galaxy.75c0119c.ttf"
+  },
+  {
+    "revision": "d3398f0de5c52c1247266b94757d2d71",
+    "url": "/starwars-plot-droide/static/media/rebellion.d3398f0d.ttf"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/starwars-plot-droide/static/media/starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "2be88c300e50834558b77aae9ae6a53a",
+    "url": "/starwars-plot-droide/static/media/starlogo.2be88c30.ttf"
+  },
+  {
+    "revision": "f81f6f7b240ab92b0d89918a96f07768",
+    "url": "/starwars-plot-droide/static/media/stars3.f81f6f7b.png"
+  },
+  {
+    "revision": "3f085be9c96d464c8ed2774e89793393",
+    "url": "/starwars-plot-droide/static/media/starwars.3f085be9.ttf"
+  }
+]);
