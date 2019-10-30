281d7da2824f6d783f5128b58464bceca48ad975
ZwergMichi242 <michaela.anzengruber@antz-development.at>
2019-10-30 17:48:11 +0100

    Updates

---
+++ precache-manifest.f216cff8f55a8601078f7d02fb038324.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "029fd27878966b3225aff72b1b9296bd",
+    "url": "/starwars-plotdroid/index.html"
+  },
+  {
+    "revision": "2a1401d7a060fd4c2ee3",
+    "url": "/starwars-plotdroid/static/css/main.ed4f783a.chunk.css"
+  },
+  {
+    "revision": "82c6cfdc7a8ca2f5d7dc",
+    "url": "/starwars-plotdroid/static/js/2.4bf34477.chunk.js"
+  },
+  {
+    "revision": "2a1401d7a060fd4c2ee3",
+    "url": "/starwars-plotdroid/static/js/main.a09c445c.chunk.js"
+  },
+  {
+    "revision": "9e1be96b93df4706e5bb",
+    "url": "/starwars-plotdroid/static/js/runtime-main.a9398ad2.js"
+  },
+  {
+    "revision": "c982ef251eeaa93f501080ca646e5e24",
+    "url": "/starwars-plotdroid/static/media/anakin.c982ef25.ttf"
+  },
+  {
+    "revision": "ade91cb0bb7c69696a7df18125ae82cb",
+    "url": "/starwars-plotdroid/static/media/deathstar.ade91cb0.otf"
+  },
+  {
+    "revision": "7ba6f280fc3117e9cd133bf1c23aa95a",
+    "url": "/starwars-plotdroid/static/media/distant.7ba6f280.ttf"
+  },
+  {
+    "revision": "a9df3614dac722fd5ee0866953e0551b",
+    "url": "/starwars-plotdroid/static/media/droid.a9df3614.png"
+  },
+  {
+    "revision": "c998b685a8fdce0dcb4f7a35277a5b80",
+    "url": "/starwars-plotdroid/static/media/droid.c998b685.ttf"
+  },
+  {
+    "revision": "10c1a1443d862233730f1f7e388e8375",
+    "url": "/starwars-plotdroid/static/media/episodeone.10c1a144.ttf"
+  },
+  {
+    "revision": "75c0119ca475dea587b8661501320be5",
+    "url": "/starwars-plotdroid/static/media/galaxy.75c0119c.ttf"
+  },
+  {
+    "revision": "d3398f0de5c52c1247266b94757d2d71",
+    "url": "/starwars-plotdroid/static/media/rebellion.d3398f0d.ttf"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/starwars-plotdroid/static/media/starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "2be88c300e50834558b77aae9ae6a53a",
+    "url": "/starwars-plotdroid/static/media/starlogo.2be88c30.ttf"
+  },
+  {
+    "revision": "f81f6f7b240ab92b0d89918a96f07768",
+    "url": "/starwars-plotdroid/static/media/stars3.f81f6f7b.png"
+  },
+  {
+    "revision": "3f085be9c96d464c8ed2774e89793393",
+    "url": "/starwars-plotdroid/static/media/starwars.3f085be9.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/starwars-plot-droide/precache-manifest.7671a67184a6353163f7435ade1f74cf.js"
+  "/starwars-plotdroid/precache-manifest.f216cff8f55a8601078f7d02fb038324.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/starwars-plot-droide/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/starwars-plotdroid/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
